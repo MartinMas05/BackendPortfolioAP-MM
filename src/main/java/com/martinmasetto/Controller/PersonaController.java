8acded185c9ad95696bec207f3196afd2204880f
MartinMas05 <martinmasetto05@gmail.com>
2023-05-06 12:01:55 -0300

    sera o no sera

--- src/main/java/com/martinmasetto/Controller/PersonaController.java
+++ src/main/java/com/martinmasetto/Controller/PersonaController.java
@@ -12,6 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -38,6 +39,13 @@ public class PersonaController {
         
         Persona persona = personaService.getOne(id).get();
         return new ResponseEntity(persona, HttpStatus.OK);
+    }
+    
+    @PostMapping("/crear")
+    public String createPersona(@RequestBody Persona persona){
+        personaService.save(persona);
+        return "la Persona fue creada correctamente";
+        
     }
     
     /*@DeleteMapping("/delete/{id}")
